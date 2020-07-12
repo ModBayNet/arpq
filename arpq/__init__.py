781363578430a03202fd0f533e25f5dd3c1c55cb
Eugene <fogaprod@gmail.com>
2020-07-12 21:53:28 +0300

    Bump version

--- arpq/__init__.py
+++ arpq/__init__.py
@@ -3,7 +3,7 @@ from .types import *  # NOQA
 from .message import Message
 from .encoders import *  # NOQA
 
-__version__ = "0.1.1"
+__version__ = "0.1.2a0"
 __author__ = "Eugene Ershov"
 
 __all__ = (
